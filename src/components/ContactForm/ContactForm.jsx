fcfe5352e267ff77b5b0589ad4a6e3b359c90c37
Protsak Petro <49090552+ProtsakPetro@users.noreply.github.com>
2023-10-13 22:46:45 +0200

    som_fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,64 +1,70 @@
-import { Component } from "react"
-import { FormButton, Form, FormInputContainer, InputField, InputLabel } from "./ContactForm.styled"
+import React, { useState } from "react";
+import {
+  FormButton,
+  Form,
+  FormInputContainer,
+  InputField,
+  InputLabel,
+} from "./ContactForm.styled";
 import { nanoid } from 'nanoid';
 
-class ContactForm extends Component {
-  state = {
-  name: '',
-  number: ''  
-  }
+function ContactForm({ addContactData }) {
+  const [formData, setFormData] = useState({
+    name: '',
+    number: ''
+  });
 
-  addContact = (e)=> {
-  e.preventDefault();
-  const newContact = {
-    name: this.state.name,
-    number: this.state.number,
-    id: nanoid()
-    }
-    this.props.addContactData(newContact, this.state); 
-    this.setState({
+  const addContact = (e) => {
+    e.preventDefault();
+    const newContact = {
+      name: formData.name,
+      number: formData.number,
+      id: nanoid()
+    };
+    addContactData(newContact, formData);
+    setFormData({
       name: '',
       number: ''
-    })
+    });
+  };
 
-  }
+  const getContactData = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
 
-  getContactData = ({ target: { name, value } }) => {
-    this.setState({[name]: value})
-  }
-
-  render() {
-    return <Form onSubmit={this.addContact}>
- 			<FormInputContainer>
-         <InputField
-           type="tel"
-           name="number"
-           pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+  return (
+    <Form onSubmit={addContact}>
+      <FormInputContainer>
+        <InputField
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           autoComplete="off"
-          onChange={this.getContactData}
-          value={this.state.number}
-           required
-         />
-         <InputLabel>Number</InputLabel>
-         <InputField
-           type="text"
-           name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-           autoComplete="off"
-            value={this.state.name}
-           onChange={this.getContactData}
-           required
-         />
-         <InputLabel>Name</InputLabel>
- 			</FormInputContainer>
- 				<FormButton type="submit">ADD CONTACTS</FormButton>
- 		</Form>
-  }
+          onChange={getContactData}
+          value={formData.number}
+          required
+        />
+        <InputLabel>Number</InputLabel>
+        <InputField
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          autoComplete="off"
+          value={formData.name}
+          onChange={getContactData}
+          required
+        />
+        <InputLabel>Name</InputLabel>
+      </FormInputContainer>
+      <FormButton type="submit">ADD CONTACTS</FormButton>
+    </Form>
+  );
+}
 
-  
-  
-
-} 
-export default ContactForm
+export default ContactForm;
