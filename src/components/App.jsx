a5aae320e8600ae525c28b3bbed16d9f7841f15b
Protsak Petro <49090552+ProtsakPetro@users.noreply.github.com>
2023-10-13 09:31:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Section from "./Section/Section";
+import Statistics from "./Statistics/Statistics";
+import { Container } from "./index.styled";
+import Notification from "./Notification/Notification";
+
+class App extends Component {
+  state = {
+    like: 0,
+    neutral: 0,
+    dislike: 0
+  };
+
+  countTotalFeedback = () => {
+    const { like, neutral, dislike } = this.state;
+    const feedbackValues = Object.values({ like, neutral, dislike });
+    return feedbackValues.reduce((total, value) => total + value, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { like } = this.state;
+    const total = this.countTotalFeedback();
+    const percentage = total === 0 ? 0 : (like / total) * 100;
+    return Math.round(percentage) + "%";
+  };
+
+  handleFeedback = (e) => {
+    const buttonName = e.target.textContent;
+    this.setState((prevState) => ({
+      [buttonName.toLowerCase()]: prevState[buttonName.toLowerCase()] + 1
+    }));
+  };
+
+  render() {
+    const { like, neutral, dislike } = this.state;
+    const total = this.countTotalFeedback(); 
+    return (
+      <Container>
+        <Section title="SHARE YOUR EXPERIENCES">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section title="STATISTIC">
+          {total > 0 ? (
+            <Statistics
+              good={like}
+              neutral={neutral}
+              bad={dislike}
+              total={total} 
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="THERE IS NO FEEDBACK" />
+          )}
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
